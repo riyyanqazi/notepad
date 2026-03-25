ef0cdc35c0a24de5d979ab004b9e4fb2aa97a443
riyyanqazi <riyyanqazi25@gmail.com>
2026-03-25 11:15:50 +0500

    add Reset button

--- notepad.js
+++ notepad.js
@@ -14,8 +14,6 @@ allbutton[0].addEventListener("click", () => {
   alert("Saved");
   let val = notePad.value;
   let a = JSON.stringify(val);
-  console.log(val);
-
   localStorage.setItem("Notes", a);
 });
 
@@ -27,22 +25,29 @@ allbutton[2].addEventListener("click", () => {
   let res = confirm("Are you sure to rest Current data Remove from notepad");
   console.log(res);
 
+
   if (res === true) {
     notePad.value = "";
   }
 });
-let  p = document.querySelector('p')
-allbutton[1].addEventListener('click',()=>{
-    let notesselection = document.querySelector('.notesselection') 
-    notesselection.style.border="2px solid white"   
-    let card= document.createElement('div')
-let context= document.createElement('p')
-context.innerText=notePad.value
-notePad.value=""
-card.style.border='2px solid green'
-notesselection.append(card)
-card.append(context)
+let p = document.querySelector("p");
+allbutton[1].addEventListener("click", () => {
+  if (notePad.value !== "") {
+    let notesselection = document.querySelector(".notesselection");
+    let card = document.createElement("div");
+    notesselection.style.border = "2px solid white";
+    let context = document.createElement("p");
+    context.innerText = notePad.value;
 
-    
+    notePad.value = "";
+    card.style.border = "2px solid green";
+    card.style.cursor = "pointer";
+    card.style.padding = "20px";
+    card.style.width = "200px";
 
-})
+    notesselection.append(card);
+    card.append(context);
+  } else {
+    alert("Create Note's");
+  }
+});
